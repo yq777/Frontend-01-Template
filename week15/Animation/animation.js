48187c81f052e40dccc4f3add7275029c4d29e08
qin.yao <qin.yao@elitesland.com>
2020-07-23 01:11:28 +0800

    feat: add week15

--- week15/Animation/animation.js
+++ week15/Animation/animation.js
@@ -26,7 +26,7 @@ export class Timeline {
           progression = 1;
           animation.finshed = true;
         }
-        let value = start + progression * (end - start);
+        let value = animation.valueFromProgression(progression);
         object[property] = template(value);
       }
       if (animations.length) {
@@ -76,7 +76,7 @@ export class Timeline {
     animation.finshed = false;
     if (this.state === 'playing') {
       animation.addTime =
-      addTime !== void 0 ? addTime : Date.now() - this.startTime;
+        addTime !== void 0 ? addTime : Date.now() - this.startTime;
     } else {
       animation.addTime = addTime !== void 0 ? addTime : 0;
     }
@@ -87,12 +87,12 @@ export class Animation {
   constructor(
     object,
     property,
-    template,
     start,
     end,
     duration,
     delay,
-    timingFunction
+    timingFunction,
+    template
   ) {
     this.object = object;
     this.template = template;
@@ -102,6 +102,41 @@ export class Animation {
     this.delay = delay || 0;
     this.timingFunction = timingFunction;
     this.duration = duration;
+  }
+
+  valueFromProgression(progression) {
+    return this.start + progression * (this.end - this.start);
+  }
+}
+
+export class ColorAnimation {
+  constructor(
+    object,
+    property,
+    start, // rgb值
+    end, // rgb值
+    duration,
+    delay,
+    timingFunction,
+    template
+  ) {
+    this.object = object;
+    this.template = template || ((v) => `rgba(${v.r}, ${v.g}, ${v.b}, ${v.a})`);
+    this.property = property;
+    this.start = start;
+    this.end = end;
+    this.delay = delay || 0;
+    this.timingFunction = timingFunction;
+    this.duration = duration;
+  }
+
+  valueFromProgression(progression) {
+    return {
+      r: this.start.r + progression * (this.end.r - this.start.r),
+      g: this.start.g + progression * (this.end.g - this.start.g),
+      b: this.start.b + progression * (this.end.b - this.start.b),
+      a: this.start.a + progression * (this.end.a - this.start.a),
+    };
   }
 }
 
